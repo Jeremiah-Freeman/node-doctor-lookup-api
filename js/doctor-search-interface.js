ac07229f3cadc5a4fbf8b41d9df22d8c0bf51edf
Jay Freeman <jaythinkshappiness@gmail.com>
2017-03-17 11:23:40 -0700

    successfully make an API call to the console

--- js/doctor-search-interface.js
+++ js/doctor-search-interface.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
     var  ailment = $("#ailment").val();
     console.log(ailment);
     $("#ailment").val("");
-    $(".showdoctors").text("Here are some doctors that may be right for you:");
+    $(".showdoctors").text("Here are some doctors that may be right for you:" + ailment);
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=cb89914800cabcde62aea182bcad9347', function(response) {
+      console.log(response.data.insurances);
+    });
   });
 });
+
+
+// cb89914800cabcde62aea182bcad9347
