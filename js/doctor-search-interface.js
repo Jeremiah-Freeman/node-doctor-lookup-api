7128b94aa492ddbac7d9b5717c5df48cde867b43
Jay Freeman <jaythinkshappiness@gmail.com>
2017-03-17 09:55:28 -0700

    store API key in .env for safe keeping - update index button class along with more code to capture value of input on interface.js

--- js/doctor-search-interface.js
+++ js/doctor-search-interface.js
@@ -1,3 +1,9 @@
+var apiKey = "cb89914800cabcde62aea182bcad9347";
 $(document).ready(function(){
-  alert("hey it works");
+  $("#finddoctors").click(function(){
+    var  ailment = $("#ailment").val();
+    console.log(ailment);
+    $("#ailment").val("");
+    $(".showdoctors").text("Here are some doctors that may be right for you:");
+  });
 });
