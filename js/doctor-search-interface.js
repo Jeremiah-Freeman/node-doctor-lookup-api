1f9cb07b1bfcc1b7c2a71d8a91faed0c93eb8987
Jay Freeman <jaythinkshappiness@gmail.com>
2017-03-17 14:01:08 -0700

    successfully return multiple queries from the API and display them on HTML

--- js/doctor-search-interface.js
+++ js/doctor-search-interface.js
@@ -1,12 +1,16 @@
 var apiKey = "cb89914800cabcde62aea182bcad9347";
+
 $(document).ready(function(){
   $("#finddoctors").click(function(){
     var  ailment = $("#ailment").val();
     console.log(ailment);
     $("#ailment").val("");
-    $(".showdoctors").text("Here are some doctors that may be right for you:" + ailment);
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=cb89914800cabcde62aea182bcad9347', function(response) {
-      console.log(response.data.insurances);
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ ailment+'&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).
+    then(function(response) {
+      console.log(response.data[0].practices[0].name);
+      $(".showdoctors").text("Here are some doctors that may be right for you:" + ailment + response.data[0].practices[0].name + response.data[1].practices[0].name + response.data[2].practices[0].name + response.data[3].practices[0].name + response.data[4].practices[0].name + response.data[5].practices[0].name +
+      response.data[6].practices[0].name +
+      response.data[7].practices[0].name + response.data[8].practices[0].name + response.data[9].practices[0].name);
     });
   });
 });
